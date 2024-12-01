4b33b94d2952d67faa9f74dfd71b6583a3bcd4ec
RoelerCoaster <RoelerCoaster@users.noreply.github.com>
2024-12-01 15:29:14 +0100

    Day 1

--- Util/JaggedArrayGridExtensions.cs
+++ Util/JaggedArrayGridExtensions.cs
@@ -10,4 +10,26 @@ internal static class JaggedArrayGridExtensions
     {
         return grid.Select(x => x.StringJoin("")).StringJoin(Environment.NewLine);
     }
+
+    public static T[][] Transpose<T>(this T[][] grid)
+    {
+        if (grid.Length == 0)
+        {
+            return grid;
+        }
+
+        var transposed = new T[grid[0].Length][];
+
+        for (var j = 0; j < transposed.Length; j++)
+        {
+            transposed[j] = new T[grid.Length];
+
+            for (var i = 0; i < grid.Length; i++)
+            {
+                transposed[j][i] = grid[i][j];
+            }
+        }
+
+        return transposed;
+    }
 }
