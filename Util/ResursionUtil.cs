5d5f67f44d2007c789fc966bf660e82a61ef06ab
RoelerCoaster <RoelerCoaster@users.noreply.github.com>
2024-12-04 22:19:10 +0100

    Day 4

---
+++ Util/ResursionUtil.cs
@@ -0,0 +1,26 @@
+﻿namespace RoelerCoaster.AdventOfCode.Year2024.Util;
+internal static class ResursionUtil
+{
+
+    public static Func<TArg, TResult> Memoize<TArg, TResult>(Func<Func<TArg, TResult>, TArg, TResult> implementation, EqualityComparer<TArg>? equalityComparer = null)
+        where TArg : notnull
+    {
+        var cache = new Dictionary<TArg, TResult>(equalityComparer ?? EqualityComparer<TArg>.Default);
+
+        TResult MemoizedImplementation(TArg arguments)
+        {
+            if (cache.TryGetValue(arguments, out var cached))
+            {
+                return cached;
+            }
+
+
+            var result = implementation(MemoizedImplementation, arguments);
+            cache.Add(arguments, result);
+
+            return result;
+        }
+
+        return MemoizedImplementation;
+    }
+}
