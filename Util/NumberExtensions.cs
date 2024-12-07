74485140ae12b988bd4bff011d6910e21a86fdbe
RoelerCoaster <RoelerCoaster@users.noreply.github.com>
2024-12-07 11:43:22 +0100

    Day 7

---
+++ Util/NumberExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+namespace RoelerCoaster.AdventOfCode.Year2024.Util;
+internal static class NumberExtensions
+{
+    public static int NumberOfDigits<T>(this T num) where T : IBinaryInteger<T>, IComparable<T>
+    {
+        var test = T.Abs(num);
+        var ten = T.CreateChecked(10);
+        var numDigits = 0;
+
+        do
+        {
+            test /= ten;
+            numDigits++;
+        } while (test.CompareTo(T.Zero) > 0);
+
+        return numDigits;
+    }
+}
