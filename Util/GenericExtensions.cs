d99070300615fc4ab2796d9e59f4e4cfec3dc3be
RoelerCoaster <RoelerCoaster@users.noreply.github.com>
2024-12-12 23:21:58 +0100

    Day 12 part 2

---
+++ Util/GenericExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace RoelerCoaster.AdventOfCode.Year2024.Util;
+public static class GenericExtensions
+{
+
+    public static bool IsIn<T>(this T item, params HashSet<T> items)
+    {
+        return items.Contains(item);
+    }
+}
